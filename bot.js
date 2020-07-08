89e694ac9aad1f3e60759fa1e1d0190cdfbd5991
István Bogdándi <ibogdandi@loginet.hu>
2020-07-08 08:15:13 +0200

    bot, env

--- bot.js
+++ bot.js
@@ -1,2 +1,18 @@
+require('dotenv').config();
+
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
